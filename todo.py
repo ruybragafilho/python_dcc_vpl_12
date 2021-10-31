b4e11d72b74d4d4ac65d25419e1f098687b0a0fe
ruybragafilho <ruybragafilho@gmail.com>
2021-10-31 20:11:14 -0300

    Versao Final

--- todo.py
+++ todo.py
@@ -1,14 +1,17 @@
 # -*- coding: utf8
 
+import os
 
 import threading
 import requests
 
 
 class Worker(threading.Thread):
+
     def __init__(self, id_, **kwargs):
         super(Worker, self).__init__(**kwargs)
-        self._id = id_ # isso é o id do livro
+        self.__id = id_ # isso é o id do livro
+        self.__numLinhas = 0
     
     def run(self):
         # Use requests.get para baixar um livro
@@ -16,10 +19,44 @@ class Worker(threading.Thread):
         # id_ = 1182
         # 'http://www.gutenberg.org/files/{}/{}-0.txt'.format(id_, id_)
         # USE HTTP PARA FUNCIONAR NO MOODLE, NÃO HTTPS
-        pass
+        url = 'http://www.gutenberg.org/files/{}/{}-0.txt'.format(self.__id, self.__id)
+        livro = requests.get( url )
+        for linha in livro.text.splitlines():
+            self.__numLinhas += 1
+
+    def get_result(self):
+        # 1. retorna o número de linhas do livro
+        #print( 'id: ', self.__id, ' Linhas: ', self.__numLinhas )
+        return self.__numLinhas
+
 
 
 def crawler():
+        
     # Dispara uma thread por id do arquivo
-    # Soma o resultado de todas
-    pass
+    # Soma o resultado de todas    
+    pathArquivo = os.path.join( '.', 'dados', 'ids.txt' )
+    thds = []
+    with open( pathArquivo ) as inputFile:
+        for line in inputFile:
+            id = int( line.strip() )
+            thds.append( Worker(id) )
+        
+    for t in thds:
+        t.start()
+    for t in thds:
+        t.join()
+    
+    somaLinhas = 0
+    for t in thds:
+        somaLinhas += t.get_result()
+    return somaLinhas
+
+
+
+print( '\n\n' )
+
+print('Teste Crawler')
+print( crawler() )
+
+print( '\n\n' )
